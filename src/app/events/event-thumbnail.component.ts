5cd61d8ad5f5164a86612ba95ec4dd5f510aec90
Strayko <strayko_lol@windowslive.com>
2020-10-21 10:56:43 +0200

    validate object property

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,14 +5,22 @@ import {Component, Input} from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-    <div>Date: {{event.date}}</div>
-    <div>Time: {{event.time}}</div>
-    <div>Price: \${{event.price}}</div>
-    <div>
-      <span>Location: {{event.location.address}}</span>
-      <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+    <h2>{{event?.name}}</h2>
+    <div>Date: {{event?.date}}</div>
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+      Time: {{event?.time}}
+      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+      <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+      <span *ngSwitchDefault>(Normal Start)</span>
     </div>
+    <div>Price: \${{event?.price}}</div>
+    <div *ngIf="event?.location">
+      <span>Location: {{event?.location?.address}}</span>
+      <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+    </div>
+      <div *ngIf="event?.imageUrl">
+        <span>Image Url: {{event?.imageUrl}}</span>
+      </div>
   </div>
   `,
   styles: [`
@@ -22,8 +30,26 @@ import {Component, Input} from '@angular/core';
     .well div {
       color: #bbb;
     }
+    .thumbnail {
+      min-height: 220px;
+    }
   `]
 })
+// 4 - Hiding and Showing Content with ngSwitch
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am') {
+      return {'color': '#003300', 'font-weight': 'bold'};
+    }
+    return {};
+  }
 }
