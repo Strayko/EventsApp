af8d7cc9c8d8417c8d4097be53d43a3f6a3fe239
Strayko <strayko_lol@windowslive.com>
2020-10-26 15:40:16 +0100

    can activate, deactivate route and 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import {ToastrService} from './common/toastr.service';
 import {EventDetailsComponent} from './events/event-details/event-details.component';
 import {appRoutes} from './routes';
 import {RouterModule} from '@angular/router';
+import {CreateEventComponent} from './events/create-event.component';
+import {Error404Component} from './errors/404.component';
+import {EventRouteActivator} from './events/event-details/event-route-activator.service';
 
 @NgModule({
   declarations: [
@@ -17,9 +20,18 @@ import {RouterModule} from '@angular/router';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState}
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
@@ -27,3 +39,10 @@ import {RouterModule} from '@angular/router';
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  }
+  return true;
+}
