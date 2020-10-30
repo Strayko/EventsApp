d335ee00340acdd8df1f05d1cd4c1004c8883fcb
Strayko <strayko_lol@windowslive.com>
2020-10-30 15:02:49 +0100

    profile validator

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -16,4 +16,4 @@ export const appRoutes: Routes = [
   {path: '', redirectTo: '/events', pathMatch: 'full'},
   {path: 'user', loadChildren: './user/user.module#UserModule'}
 ];
-// 7 - Validating Template-based Forms
+// 7 - Validating Reactive Forms
