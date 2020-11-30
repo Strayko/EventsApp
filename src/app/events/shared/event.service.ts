75a18e08a18a67ef659998204ccaef82ffe82fd3
Strayko <strayko_lol@windowslive.com>
2020-11-30 11:16:22 +0100

    post, put, queryString and delete using http

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,7 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
-import {Observable, of, Subject} from 'rxjs';
+import {Observable, of, pipe, Subject} from 'rxjs';
 import {IEvent, ISession} from './event.model';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 
 @Injectable()
@@ -16,39 +16,18 @@ export class EventService {
 
   getEvent(id: number): Observable<IEvent> {
     return this.http.get<IEvent>('/api/events/' + id)
-      .pipe(catchError(this.handleError<IEvent>('getEvents')))
+      .pipe(catchError(this.handleError<IEvent>('getEvent')))
   }
 
   saveEvent(event) {
-    event.id = 999
-    event.session = []
-    EVENTS.push(event)
+    let options = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+    return this.http.post<IEvent>('/api/events', event, options)
+      .pipe(catchError(this.handleError<IEvent>('saveEvent')))
   }
 
-  updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id);
-    EVENTS[index] = event;
-  }
-
-  searchSessions(searchTerm: string) {
-    let term = searchTerm.toLocaleLowerCase()
-    let results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      let matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      })
-
-      results = results.concat(matchingSessions);
-    })
-
-    let emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results)
-    }, 100)
-    return emitter
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')))
   }
 
   private handleError<T> (operation = 'operation', result?: T) {
